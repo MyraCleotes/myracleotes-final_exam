0d865dd334342f8f79442cee898f5b3a24a55b42
MyraCleotes <myracleotes5@gmail.com>
2025-01-09 12:06:12 +0800

    Delete src/app/app.routes.ts

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DefaultRouteComponent } from './questions-answer/default-route.component';
-import { SecondRouteComponent } from './second-route/second-route.component';
-
-export const routes: Routes = [
-  { path: 'index', component: DefaultRouteComponent },
-  { path: 'signup', component: SecondRouteComponent },
-  { path: '', redirectTo: '/index', pathMatch: 'full' },
-];
